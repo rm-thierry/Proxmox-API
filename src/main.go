b0e3a5542afb7a98ebb10f3b03d47c50cc1faabb
Retaxo <105597296+sudo-Tiger@users.noreply.github.com>
2025-01-11 20:28:11 +0100

    update main.go

--- src/main.go
+++ src/main.go
@@ -1,24 +1,5 @@
 package main
 
-import (
-	"encoding/json"
-	"fmt"
-	"log"
-	"rm-thierry/Proxmox-API/src/handlers"
-	"rm-thierry/Proxmox-API/src/manager"
-)
-
 func main() {
-	apiManager := manager.NewAPIManager()
-	node := "pve"
-	vmid := "100"
-
-	vmDetails, err := handlers.GetVM(apiManager, node, vmid)
-	if err != nil {
-		log.Fatalf("Error fetching VM details: %v", err)
-	}
-
-	JSON, _ := json.MarshalIndent(vmDetails, "", "  ")
-
-	fmt.Printf("VM Details: %s\n", JSON)
+	// apiManager := manager.NewAPIManager()
 }
