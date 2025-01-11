000ebef6ceb201c9ed4afca4e73301feb92f2f83
Retaxo <105597296+sudo-Tiger@users.noreply.github.com>
2025-01-11 20:54:31 +0100

    getNode

--- src/handlers/nodes.go
+++ src/handlers/nodes.go
@@ -30,3 +30,28 @@ func GetNodes(apiManager *manager.APIManager) ([]map[string]interface{}, error) 
 
 	return nodes, nil
 }
+
+func GetNode(apiManager *manager.APIManager, node string) (map[string]interface{}, error) {
+	response, err := apiManager.ApiCall("GET", fmt.Sprintf("/nodes/%s", node), nil)
+	if err != nil {
+		return nil, fmt.Errorf("error performing API call: %w", err)
+	}
+
+	var result map[string]interface{}
+	if err := json.Unmarshal(response, &result); err != nil {
+		return nil, fmt.Errorf("error parsing JSON response: %w", err)
+	}
+
+	data, ok := result["data"].([]interface{})
+	if !ok {
+		return nil, fmt.Errorf("unexpected response format: 'data' field is missing or incorrect")
+	}
+
+	if len(data) > 0 {
+		if nodeData, ok := data[0].(map[string]interface{}); ok {
+			return nodeData, nil
+		}
+	}
+
+	return nil, fmt.Errorf("no valid node data found")
+}
--- src/main.go
+++ src/main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"rm-thierry/Proxmox-API/src/handlers"
+	"rm-thierry/Proxmox-API/src/manager"
+)
+
 func main() {
-	// apiManager := manager.NewAPIManager()
+	apiManager := manager.NewAPIManager()
+
+	node, err := handlers.GetNode(apiManager, "pve")
+	if err != nil {
+		log.Fatalf("Error getting node: %v", err)
+	}
+
+	JSONNODES, err := json.MarshalIndent(node, "", "    ")
+	if err != nil {
+		log.Fatalf("Error marshaling node data: %v", err)
+	}
+
+	fmt.Println(string(JSONNODES))
 }
