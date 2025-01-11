5198359a99859e418eafbd38984c4bdac18e5cf5
Retaxo <105597296+sudo-Tiger@users.noreply.github.com>
2025-01-11 23:30:41 +0100

    VM Create

--- src/main.go
+++ src/main.go
@@ -10,16 +10,18 @@ import (
 
 func main() {
 	apiManager := manager.NewAPIManager()
+	config := handlers.NewDefaultVMConfig()
+	config.VMID = "111"
+	config.Name = "test-vm"
+	config.Cores = "2"
+	config.Memory = "4096"
+	config.ISO = "local:iso/debian-12.8.0-amd64-netinst.iso"
 
-	node, err := handlers.GetNode(apiManager, "pve")
+	result, err := handlers.CreateVM(apiManager, config)
 	if err != nil {
-		log.Fatalf("Error getting node: %v", err)
+		log.Fatalf("Error creating VM: %v", err)
 	}
 
-	JSONNODES, err := json.MarshalIndent(node, "", "    ")
-	if err != nil {
-		log.Fatalf("Error marshaling node data: %v", err)
-	}
-
+	JSONNODES, _ := json.MarshalIndent(result, "", " ")
 	fmt.Println(string(JSONNODES))
 }
